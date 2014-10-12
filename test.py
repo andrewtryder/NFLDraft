7fd6c12fa6d7a859b7e6361e7c976c3fcd8342b0
spline <reticulatingspline@github.com>
2014-10-12 06:52:28 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ from supybot.test import *
 
 class NFLDraftTestCase(PluginTestCase):
     plugins = ('NFLDraft',)
+    
+    def testNFLDraft(self):
+        self.assertNotError('draftchannel add #test')
+        self.assertNotError('draftchannel del #test')
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
