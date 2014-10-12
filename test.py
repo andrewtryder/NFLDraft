3942ddcbdd06ae4125da348a93d81b21449d4197
spline <reticulatingspline@github.com>
2014-10-12 06:57:20 -0400

    More tests.

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ class NFLDraftTestCase(PluginTestCase):
     def testNFLDraft(self):
         self.assertNotError('draftchannel add #test')
         self.assertNotError('draftchannel del #test')
+        self.assertError('drafton')
+        self.assertError('draftoff')
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
